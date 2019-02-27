08a28005840cfdad7dcfca03b082feaee4ab0a61
alephomega <johan@valuepotion.com>
2019-02-27 13:25:09 +0900

    Attribute data type

---
+++ src/main/java/com/kakaopage/crm/extraction/ra/DataType.java
@@ -0,0 +1,17 @@
+package com.kakaopage.crm.extraction.ra;
+
+public enum DataType {
+    BYTE,
+    SHORT,
+    INTEGER,
+    LONG,
+    FLOAT,
+    DOUBLE,
+    DECIMAL,
+    STRING,
+    BOOLEAN,
+    TIMESTAMP,
+    DATE,
+    ARRAY,
+    MAP
+}
