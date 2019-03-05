47742964b209c7398ce41cab50df6937b912b7ef
alephomega <johan@valuepotion.com>
2019-03-05 10:51:16 +0900

    Code Refactoring

--- src/main/java/com/kakaopage/crm/extraction/Sink.java
+++ src/main/java/com/kakaopage/crm/extraction/Sink.java
@@ -4,10 +4,12 @@ import com.kakaopage.crm.extraction.ra.Relation;
 
 public class Sink implements Step {
     private final String name;
+    private final Partitioning partitioning;
     private final Relation relation;
 
-    public Sink(String name, Relation relation) {
+    public Sink(String name, Partitioning partitioning, Relation relation) {
         this.name = name;
+        this.partitioning = partitioning;
         this.relation = relation;
     }
 
@@ -17,5 +19,9 @@ public class Sink implements Step {
 
     public Relation getRelation() {
         return relation;
+    }
+
+    public Partitioning getPartitioning() {
+        return partitioning;
     }
 }
