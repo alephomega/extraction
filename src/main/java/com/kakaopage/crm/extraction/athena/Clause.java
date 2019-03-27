bfce3239b8de208be4ebfb3f5f253932dacd5be0
alephomega <johan@valuepotion.com>
2019-03-27 10:26:54 +0900

    Target Extraction

--- src/main/java/com/kakaopage/crm/extraction/athena/Clause.java
+++
@@ -1,5 +0,0 @@
-package com.kakaopage.crm.extraction.athena;
-
-public abstract class Clause {
-    public abstract String stringify(StatementContext context);
-}
--- src/main/java/com/kakaopage/crm/extraction/athena/StatementDecorator.java
+++
@@ -1,7 +0,0 @@
-package com.kakaopage.crm.extraction.athena;
-
-import com.kakaopage.crm.extraction.ra.RelationalAlgebraOperator;
-
-public interface StatementDecorator<S extends Statement, T extends RelationalAlgebraOperator> {
-    S build(S statement, T operation);
-}
