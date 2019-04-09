481151da0e6b9f446cdcdeee4ba1d74a2cfe6e9e
alephomega <johan@valuepotion.com>
2019-04-10 00:38:39 +0900

    Target Extraction

--- src/main/java/com/kakaopage/crm/extraction/ra/Selection.java
+++ src/main/java/com/kakaopage/crm/extraction/ra/Selection.java
@@ -2,22 +2,17 @@ package com.kakaopage.crm.extraction.ra;
 
 import com.kakaopage.crm.extraction.Predicate;
 import com.kakaopage.crm.extraction.Symbol;
-import com.kakaopage.crm.extraction.ra.relations.Source;
 
 @Symbol("σ")
 public class Selection extends UnaryRelationalAlgebraOperator {
     private final Predicate condition;
 
-    public Selection(Predicate condition, Source source) {
-        super(source);
+    public Selection(Predicate condition, Relation relation) {
+        super(relation);
         this.condition = condition;
     }
 
     public Predicate getCondition() {
         return condition;
-    }
-
-    public Source getSource() {
-        return (Source) getRelation();
     }
 }
