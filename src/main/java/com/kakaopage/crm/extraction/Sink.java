7f659cc9a3c22e5f1ae21c673370e46749694a82
alephomega <johan@valuepotion.com>
2019-04-11 13:18:28 +0900

    Target Extraction

--- src/main/java/com/kakaopage/crm/extraction/Sink.java
+++ src/main/java/com/kakaopage/crm/extraction/Sink.java
@@ -3,16 +3,30 @@ package com.kakaopage.crm.extraction;
 import com.kakaopage.crm.extraction.ra.Relation;
 
 public class Sink implements Expression {
+    private final Sampling sampling;
     private final Partitioning partitioning;
     private final Relation relation;
 
     public Sink(Partitioning partitioning, Relation relation) {
+        this(null, partitioning, relation);
+    }
+
+    public Sink(Sampling sampling, Partitioning partitioning, Relation relation) {
+        this.sampling = sampling;
         this.partitioning = partitioning;
         this.relation = relation;
     }
 
     public Relation getRelation() {
         return relation;
+    }
+
+    public boolean needSampling() {
+        return sampling != null && sampling.getSize() > 0;
+    }
+
+    public Sampling getSampling() {
+        return sampling;
     }
 
     public Partitioning getPartitioning() {
